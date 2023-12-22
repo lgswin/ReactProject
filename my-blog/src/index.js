bfd594a0745d628845fbb03097e1867ebf36710e
lgswin <lgswin@gmail.com>
2023-12-21 22:18:38 -0500

    firebase/auth, login function

--- my-blog/src/index.js
+++ my-blog/src/index.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { initializeApp } from "firebase/app";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const firebaseConfig = {
+  apiKey: "AIzaSyAFHzM9kJ5bcc9kjFq-eFLYiKRje4NsFpo",
+  authDomain: "my-react-blog-c72ab.firebaseapp.com",
+  projectId: "my-react-blog-c72ab",
+  storageBucket: "my-react-blog-c72ab.appspot.com",
+  messagingSenderId: "712961342621",
+  appId: "1:712961342621:web:4164d4e6cf4c3dd467a874",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
